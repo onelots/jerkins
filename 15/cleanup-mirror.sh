c871cb6eb08433bee7f6f8dfb2c5139f945c89d0
Onelots <onelots@onelots.fr>
2025-06-15 00:38:30 +0200

    jenkins: add a script to automatically cleanup the mirror when the time comes
    Signed-off-by: Onelots <onelots@onelots.fr>

---
+++ 15/cleanup-mirror.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf /root/Testers/
+mkdir /root/Testers/
+
+version=$1
+
+devices="Sargo Bonito Crosshatch Blueline Cheeseburger Dumpling Guacamole Guacamoleb Hotdog Hotdogb Miatoll Laurel_Sprout Veux Vince Scorpio Polaris"
+for device in $devices; do
+    mkdir /root/Testers/$device
+    mkdir /root/Testers/$device/$version     
+done
