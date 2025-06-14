95e8b581d16e54fe9c83a8a75a72085d851247c7
Onelots <onelots@onelots.fr>
2025-06-14 15:47:12 +0200

    jenkins: 50G is way enough for my case.
    Signed-off-by: Onelots <onelots@onelots.fr>

--- 15/03-clean.sh
+++ 15/03-clean.sh
@@ -4,7 +4,7 @@ device=$1
 
 target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
 
-export CCACHE_MAXSIZE=300G
+export CCACHE_MAXSIZE=50G
 
 source build/envsetup.sh &&
 lunch lineage_$device-$target-userdebug &&
--- 15/04-build.sh
+++ 15/04-build.sh
@@ -5,7 +5,7 @@ device=$1
 target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
 
 export EVO_BUILD_TYPE=Official
-export CCACHE_MAXSIZE=300G
+export CCACHE_MAXSIZE=50G
 
 source build/envsetup.sh &&
 lunch lineage_$device-$target-user &&
