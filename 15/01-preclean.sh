00e8207ce26fa160ea17cadcd281e53864f1eeff
Onelots <onelots@onelots.fr>
2025-06-14 15:45:37 +0200

    jenkins: oems are not devices
    Signed-off-by: Onelots <onelots@onelots.fr>

--- 15/01-preclean.sh
+++ 15/01-preclean.sh
@@ -4,8 +4,8 @@ if  [ -f ".repo/local_manifests/roomservice.xml" ]; then
     rm .repo/local_manifests/roomservice.xml
 fi
 
-devices="asus xiaomi realme motorola miromax wingtech oneplus lenovo samsung google"
-for device in $devices; do
+oems="asus xiaomi realme motorola miromax wingtech oneplus lenovo samsung google"
+for device in $oems; do
     if [[ -d device/$device ]]; then rm -rf device/$device; fi
     if [[ -d kernel/$device ]]; then rm -rf kernel/$device; fi
     if [[ -d vendor/$device ]]; then rm -rf vendor/$device; fi
