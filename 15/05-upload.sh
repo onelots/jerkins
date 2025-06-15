4486201f36d2515ed66e15466b758f1521491bec
Onelots <onelots@onelots.fr>
2025-06-15 23:26:29 +0200

    jenkins: adapt to my personal usage
    Signed-off-by: Onelots <onelots@onelots.fr>

--- 15/05-upload.sh
+++ 15/05-upload.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Defines all our useful variables
 device=$1
+device_capitalized=$(echo $device | sed -E 's/(^|_)([a-z])/\1\u\2/g')
+user=$2
+host=$3
+private_key=$4
 
 # Extract Android Version from json
 filename=$(echo out/target/product/$device/EvolutionX-*.zip)
-version=$(echo $filename | cut -d "-" -f 2)
+android_version=$(echo $filename | cut -d "-" -f 2)
+build_date=$(echo $filename | cut -d "-" -f 3)
+evo_version=$(echo $filename | cut -d "-" -f 5)
+evo_version_official=${evo_version}+="-Official"
+
+# Check if folder exists
+# Build_date's path (example : /root/Testers/Polaris/15/10.7/20250515/)
+build_date_path = ("/root/Testers/${device_capitalized}/${android_version}/${evo_version_official}/${build_date}")
+ssh ${user}@${host} "[ -d ${evo_version_path} ] && echo 'exists' || mkdir -p  ${evo_version_path}"
 
 # Upload main rom
 echo "Uploading main rom..."
-rclone copy out/target/product/$device/EvolutionX*.zip b2:evo-downloads/$device/$version/
+scp -o StrictHostKeyChecking=no -i ${private_key} out/target/product/$device/EvolutionX*.zip ${user}@${host}:/${build_date_path}
+                 
 echo " "
 
 # Identify and upload initial install images
@@ -20,6 +34,6 @@ initial_images=$(jq -r '.response[0].initial_installation_images[]' "$json")
 # Upload found images
 for image in $initial_images; do
     echo "Uploading $image..."
-    # Nothing for now, gotta see if it works first...
+    scp -o StrictHostKeyChecking=no -i ${private_key} out/target/product/$device/$image ${user}@${host}:/${build_date_path}
     echo " "
 done
