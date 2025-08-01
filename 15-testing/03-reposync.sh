5c65913953760941e4e3c83ab60be4715edcd5d6
Onelots <onelots@onelots.fr>
2025-08-01 20:52:04 +0200

    jenkins: scripts: separate release and testing scripts
    Signed-off-by: Onelots <onelots@onelots.fr>

---
+++ 15-testing/03-reposync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+device=$1
+target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
+
+source build/envsetup.sh &&
+lunch lineage_$device-$target-userdebug &&
+repo sync "${@:2}"
---
+++ 15-testing/04-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+device=$1
+
+target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
+
+export EVO_BUILD_TYPE=Official
+export CCACHE_MAXSIZE=50G
+
+source build/envsetup.sh &&
+lunch lineage_$device-$target-userdebug &&
+m installclean &&
+m evolution
