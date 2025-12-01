2973c1751f5ce6919b0ed3c35cf48f0ecd9f094c
Onelots <onelots@onelots.fr>
2025-12-01 02:34:12 +0100

    jerkins: cleanup init
    Signed-off-by: Onelots <onelots@onelots.fr>

--- vars/sync.groovy
+++ vars/sync.groovy
@@ -13,62 +13,70 @@ def call(Map config = [:]) {
 
         stages {
 
-            withCredentials([
-                string(credentialsId: 'discord-webhook', variable: 'DISCORD_WEBHOOK')
-            ]) {
-
-                stage('Get Folder Name') {
-                    steps {
+            stage('Get Folder Name') {
+                steps {
+                    withCredentials([
+                        string(credentialsId: 'discord-webhook', variable: 'DISCORD_WEBHOOK')
+                    ]) {
                         script {
                             folderName  = pwd().split('/')[-2]
                             folderParts = folderName.split(' - ')
+                            evoVersion  = folderParts[0].split('\\.')[0]
 
-                            evoVersion = folderParts[0].split('\\.')[0]
-                            echo "Evo Version   : ${evoVersion}"
+                            echo "Evo Version : ${evoVersion}"
                         }
                     }
                 }
+            }
 
-                stage('Initialize EvolutionX') {
-                    steps {
-                        dir("/media/sauces/evo${evoVersion}") {
-                            withEnv(["evoVersion=${evoVersion}"]) {
-                                sh '''
-                                    /media/sauces/scripts/shell/init.sh ${evoVersion}
-                                '''
+            stage('Initialize EvolutionX') {
+                steps {
+                    withCredentials([
+                        string(credentialsId: 'discord-webhook', variable: 'DISCORD_WEBHOOK')
+                    ]) {
+                        script {
+                            dir("/media/sauces/evo${evoVersion}") {
+                                sh "/media/sauces/scripts/shell/init.sh ${evoVersion}"
                             }
                         }
                     }
                 }
-
             }
-        } 
+        }
+
         post {
 
             success {
-                script {
-                    discordSend(
-                        description: "Build environment initialize/updated !",
-                        footer: 'Jenkins Pipeline',
-                        link: env.BUILD_URL,
-                        result: currentBuild.currentResult,
-                        title: JOB_NAME,
-                        webhookURL: DISCORD_WEBHOOK // Kek no more webhook directly in code (kill me please)
-                    )
+                withCredentials([
+                    string(credentialsId: 'discord-webhook', variable: 'DISCORD_WEBHOOK')
+                ]) {
+                    script {
+                        discordSend(
+                            description: "Build environment initialize/updated !",
+                            footer: 'Jenkins Pipeline',
+                            link: env.BUILD_URL,
+                            result: currentBuild.currentResult,
+                            title: JOB_NAME,
+                            webhookURL: DISCORD_WEBHOOK
+                        )
+                    }
                 }
             }
 
             failure {
-                discordSend(
-                    description: "Build environment initialization/update failed.",
-                    footer: 'Jenkins Pipeline',
-                    link: env.BUILD_URL,
-                    result: currentBuild.currentResult,
-                    title: JOB_NAME,
-                    webhookURL: DISCORD_WEBHOOK // Kek no more webhook directly in code (kill me please)
-                )
+                withCredentials([
+                    string(credentialsId: 'discord-webhook', variable: 'DISCORD_WEBHOOK')
+                ]) {
+                    discordSend(
+                        description: "Build environment initialization/update failed.",
+                        footer: 'Jenkins Pipeline',
+                        link: env.BUILD_URL,
+                        result: currentBuild.currentResult,
+                        title: JOB_NAME,
+                        webhookURL: DISCORD_WEBHOOK
+                    )
+                }
             }
-
         }
-    } 
+    }
 }
