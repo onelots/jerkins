c67b478f76f09c52f15c0dc904db01faf725c95a
Onelots <onelots@lineageos.org>
2026-06-26 10:40:18 +0200

    jerkins: build: Export necessary vars for bazel-remote
    Signed-off-by: Onelots <onelots@lineageos.org>

--- shell/build.sh
+++ shell/build.sh
@@ -10,6 +10,32 @@ export CCACHE_MAXSIZE=70G
 
 echo " "
 echo " "
+echo "Exporting RBE vars"
+# RBE
+export USE_RBE=1
+
+export RBE_CXX_EXEC_STRATEGY=local
+export RBE_D8_EXEC_STRATEGY=local
+export RBE_JAVAC_EXEC_STRATEGY=local
+export RBE_METALAVA_EXEC_STRATEGY=local
+export RBE_R8_EXEC_STRATEGY=local
+export RBE_RUST_EXEC_STRATEGY=local
+
+export RBE_CXX=1
+export RBE_D8=1
+export RBE_JAVAC=1
+export RBE_METALAVA=1
+export RBE_R8=1
+export RBE_RUST=1
+
+export RBE_instance=evolutionx
+export RBE_service=127.0.0.1:9092
+export RBE_service_no_security=true
+export RBE_service_no_auth=true
+export RBE_DIR=prebuilts/remoteexecution-client/live
+
+echo " "
+echo " "
 echo "Building EvolutionX for $device"
 echo "------------------------------------------"
 
