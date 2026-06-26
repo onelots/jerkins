4d0d5cdff935e64def7c20538bfec038915f324d
Onelots <onelots@lineageos.org>
2026-06-26 10:28:47 +0200

    jerkins: init: pull bazel-remote
    Signed-off-by: Onelots <onelots@lineageos.org>

--- shell/init.sh
+++ shell/init.sh
@@ -13,6 +13,19 @@ else
     exit 1
 fi
 
+echo " "
+echo "----------------------------------------------------"
+echo "Pull Bazel-remote from github releases"
+echo "----------------------------------------------------"
+
+rm -rf bin/bazel-remote
+wget https://github.com/buchgr/bazel-remote/releases/download/v2.6.1/bazel-remote-2.6.1-linux-amd64 -O bin/bazel-remote
+chmod +x bin/bazel-remote
+
+echo "----------------------------------------------------"
+echo "bazel-remote initialized, syncing EvolutionX"
+echo "----------------------------------------------------"
+
 source build/envsetup.sh
 repo init -u https://github.com/Evolution-X/manifest -b "$branch" --git-lfs
 repo sync -c -j"$(nproc --all)" --force-sync --no-clone-bundle --no-tags
